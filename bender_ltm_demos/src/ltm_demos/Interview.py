301accfa1aff54702f47773b275280dcb49a3200
Luz Martinez <lmartine@hpc.cl>
2018-10-29 02:50:30 -0300

    package bender_ltm_demos with interview demo

--- bender_ltm_demos/src/ltm_demos/Interview.py
+++ bender_ltm_demos/src/ltm_demos/Interview.py
@@ -11,7 +11,7 @@ else:
     from maqui_skills import robot_factory
     
 # #State Machines
-from uchile_states.perception import personal_information, facial_features_recognition
+from uchile_states.perception import personal_information, facial_features_recognition, emotion_recognition
 from uchile_states.perception import wait_face_detection
 
 #States
@@ -22,17 +22,25 @@ from uchile_states.head.states import LookFront, LookHome, LookPerson, LookCrowd
 from ltm_demos import AskNameSimple
 
 #MSG
-from bender_msg.msg import 
+from bender_ltm_plugins.msg import HumanEntity
 
 class PublishInformation(smach.State):
     def __init__(self,robot):
         smach.State.__init__(self, outcomes = ['succeeded','failed','aborted','preempted'],
-                    input_keys=['features','facial_features'],
+                    input_keys=['operator_name','features','facial_features', 'facial_features_image','face_emotion'],
                     output_keys=['features','facial_features'])
         self.robot=robot
-        self._ltm_topic = "/asdssd_asdd"
+        self._ltm_topic = "/bender/ltm/entity/human/update"
+        self.ltm_pub = rospy.Publisher(self._ltm_topic, HumanEntity, queue_size=10)
+
 
     def execute(self, userdata):
+
+        entity = HumanEntity()
+        entity.name = userdata.operator_name
+        entity.face = userdata.facial_features_image
+        entity.emotion = userdate.face_emotion
+
         gender, phase = [0]*2
         age  = ""
                 
@@ -40,35 +48,29 @@ class PublishInformation(smach.State):
             if 'gender' in userdata.features and len(userdata.facial_features['gender'])>0:
                 gender = userdata.facial_features['gender'][0]
                 gender = gender.lower() 
+                entity.genre = HumanEntity.MASCULINE
+                if gender == "female":
+                    entity.genre = HumanEntity.FEMININE
 
             if 'age' in userdata.features and len(userdata.facial_features['age'])>0:
-                if self.robot.name == 'bender':
-                    age_range = userdata.facial_features['age'][0]
+                
+                age_range = userdata.facial_features['age'][0]
 
-                    age = age_range.strip("()")
-                    age = age.replace(',','')
-                    age = int(age.split(" ")[0]) + (int(age.split(" ")[1])-int(age.split(" ")[0]))/2
-                    
-                    if age_range == "(0, 2)" or  age_range == "(4, 6)" or  age_range == "(8, 12)":
-                        phase="child"
-                        if gender == "female":
-                            phase="girl"
-                    else:
-                        phase="adult"
+                age = age_range.strip("()")
+                age = age.replace(',','')
+                age = int(age.split(" ")[0]) + (int(age.split(" ")[1])-int(age.split(" ")[0]))/2
+
+                entity.age_bottom = int(age.split(" ")[0])
+                entity.age_top = int(age.split(" ")[1])
+                entity.age_avg = age
+
+                if age_bottom <= 12:
+                    entity.live_phase = HumanEntity.CHILD
                 else:
-                    age = userdata.facial_features['age'][0]
-                    if age < 13:
-                        phase="child"
-                        if gender == "female":
-                            phase="girl"
-                    else:
-                        phase="adult"
+                    entity.live_phase = HumanEntity.ADULT
 
 
-            personal_information  = {'personal_gender': str(gender),
-                                    'personal_age': str(age),                                 
-                                    'personal_phase': str(phase)  }
-            #print personal_information
+            self.ltm_pub.publish(entity)
 
             return 'succeeded'
         
@@ -77,11 +79,11 @@ class PublishInformation(smach.State):
             return 'preempted'
 
 
-def getInstance(robot):
-    sm = smach.StateMachine(outcomes=['succeeded','aborted','preempted'],
-                            input_keys=['features','facial_features'])
 
-    sm.userdata.input_text = ""
+def getInstance(robot):
+    sm = smach.StateMachine(outcomes=['succeeded','aborted','preempted'])
+
+    sm.userdata.operator_name = ""
     sm.userdata.features = ['gender','age']
 
     with sm:
@@ -105,14 +107,20 @@ def getInstance(robot):
         #CAmbiar a que funcione por teclado o colocar alternativa
         smach.StateMachine.add('ASK_NAME',AskNameSimple.getInstance(robot),
             transitions={
-                'succeeded': 'PERSONAL_INFORMATION'
+                'succeeded': 'GET_INFORMATION'
             }
-        ) #operator_name
+        ) 
 
         smach.StateMachine.add('GET_INFORMATION', facial_features_recognition.getInstance(robot),
             transitions={
-                'succeeded':'ANALIZEFACEINFORMATION'})
-
+                'succeeded':'GET_EMOTION'
+                }
+        )
+        smach.StateMachine.add('GET_EMOTION', emotion_recognition.getInstance(robot),
+            transitions={
+                'succeeded':'PUBLISH_INFORMATION'
+                }
+        )
         smach.StateMachine.add('PUBLISH_INFORMATION', PublishInformation(robot))
 
         return sm
